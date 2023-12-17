77ae1ad6ea0123ac48e4542646864570ab6eee03
pepper11com <pepperherman3@gmail.com>
2023-12-17 20:37:55 +0100

    back

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-
-    id("org.sonarqube") version "4.4.1.3373"
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "pepper11com_bookish-octo-fiesta_AYx4drJ-z3Klx-XHyUzv")
-        property("sonar.projectName", "bookish-octo-fiesta")
-    }
 }
